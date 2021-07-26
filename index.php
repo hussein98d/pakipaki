890ea8ab08d4c4e8652d0684203a421535096bd4
hussein98d <daher-hussein@hotmail.fr>
2021-07-26 12:09:49 +0000

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-
+<html>
 <script>urlp=[];s=location.toString().split('?');s=s[1].split('&');for(i=0;i<s.length;i++){u=s[i].split('=');urlp[u[0]]=u[1];}</script>
 <script>alert(urlp["Identity"])</script>
 <script>alert(urlp["Expiration"])</script>
 <script>alert(urlp["Token"])</script>
 <script>alert(urlp["Roles"])</script>
 
+</html>
